ee1c1c986f26a5ce052cd19920c4f376231854c8
Full Stack <Full Stack@DESKTOP-N8VIHSJ>
2022-11-22 16:42:34 +0100

    Form add prj presque fini (manque validateur dt in out)

--- src/app/project/add-project/add-project.component.ts
+++ src/app/project/add-project/add-project.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import * as dayjs from 'dayjs';
+import { concatWith } from 'rxjs';
 
 @Component({
   selector: 'app-add-project',
@@ -13,27 +15,54 @@ export class AddProjectComponent implements OnInit {
   constructor(private _builder : FormBuilder) {
     //On crée un nouveau formulaire grâce à notre FormBuilder et on le stocke dans notre propriété registerForm
     this.registerForm = this._builder.group({
-      PrjName : [null, [Validators.required, Validators.minLength(2), Validators.maxLength(100)]],
-      PrjDescription : [null, [Validators.required, Validators.minLength(2), Validators.maxLength(1000)]],
-    })
+      PrjName : [null, [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
+      PrjDescription : [null, [Validators.required, Validators.minLength(30), Validators.maxLength(1000)]],
+      PrjGoal:[null,[Validators.required, Validators.min(1)]],
+      PrjDtIn:[null,[Validators.required]],
+      PrjDtOut:[null,[Validators.required]]
+    }, { validators : [ this.ValidatorDateDiff ]})
    }
-
   ngOnInit(): void {
   }
-  register()
-  {
+
+//Verification de la date entrée
+ChkDtIn()
+{
+
+}
+ChkDtOut()
+{
+
+}
+//si la diff est plus grande que le nombre d année passer en param sa renvoi false
+ValidatorDateDiff(form : FormGroup)    {
+    let dt_in=dayjs(form.value['PrjDtIn']);
+    let dt_out=dayjs(form.value['PrjDtOut']);
+    if(((dt_out.diff(dt_in))/1000)>(31536000*5)){
+       return { max5yr : true }
+      }
+    else {
+      return null
+    }
+    }
+  //Envoi du form vers le service 
+  sendformprj(){
     if(this.registerForm.invalid){
       //Si l'entièreté du formulaire est invalide, on va marquer tous les champos comme touchés
       this.registerForm.markAllAsTouched();
     }
     else {
       console.log(this.registerForm.value);
-      //Construit un object json avec toutes les valeurs du formulaire
-      console.log(new Date(this.registerForm.value.birthdate));
 
+      //Construit un object json avec toutes les valeurs du formulaire
       //A faire normalement avec une db :
       //let user = new User(...this.registerForm.value); //Crée l'objet à insérer en db
       //this._authService.register(user);
-    }
+       
+    }    
+
+
   }
+
+
 }
